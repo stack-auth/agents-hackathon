8797bcbe7dcbd710dd91fd0e901060bf66cc0263
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-08-09 14:58:09 -0700

    no more bounce

--- viberacer/app/home/page.tsx
+++ viberacer/app/home/page.tsx
@@ -132,7 +132,7 @@ export default function HomePage() {
         </div>
         
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 animate-bounce">
+        <div className="absolute bottom-8">
           <svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
           </svg>
